2626cb8f7498020960adacfe4ae66ab6b9891b46
Bill <billhu403@gmail.com>
2025-04-13 22:10:45 -0700

    new

--- asgn1/ColoredPoints.js
+++ asgn1/ColoredPoints.js
@@ -1,31 +1,42 @@
 // ColoredPoint.js (c) 2012 matsuda
 // Vertex shader program
-var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
-  'void main() {\n' +
-  '  gl_Position = a_Position;\n' +
-  '  gl_PointSize = 10.0;\n' +
-  '}\n';
+var VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  uniform float u_Size;
+  void main() {
+    gl_Position = a_Position;
+    gl_PointSize = u_Size;
+  }`
 
 // Fragment shader program
-var FSHADER_SOURCE =
-  'precision mediump float;\n' +
-  'uniform vec4 u_FragColor;\n' +  // uniform変数
-  'void main() {\n' +
-  '  gl_FragColor = u_FragColor;\n' +
-  '}\n';
+var FSHADER_SOURCE = `
+  precision mediump float;
+  uniform vec4 u_FragColor;
+  void main() {
+    gl_FragColor = u_FragColor; 
+  }`
 
-function main() {
+//global
+let canvas;
+let gl;
+let a_Position;
+let u_FragColor;
+let u_Size;
+
+function setupWebGL(){
   // Retrieve <canvas> element
-  var canvas = document.getElementById('webgl');
+  canvas = document.getElementById('webgl');
 
   // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
+  gl = canvas.getContext("webgl", { preserveDrawingBuffer: true});
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
   }
 
+}
+
+function connectVariablesToGLSL(){
   // Initialize shaders
   if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
     console.log('Failed to intialize shaders.');
@@ -33,21 +44,62 @@ function main() {
   }
 
   // // Get the storage location of a_Position
-  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  a_Position = gl.getAttribLocation(gl.program, 'a_Position');
   if (a_Position < 0) {
     console.log('Failed to get the storage location of a_Position');
     return;
   }
 
   // Get the storage location of u_FragColor
-  var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
   if (!u_FragColor) {
     console.log('Failed to get the storage location of u_FragColor');
     return;
   }
 
+  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+  if (!u_Size) {
+    console.log('Failed to get the storage location of u_Size');
+    return;
+  }
+
+}
+
+const POINT = 0;
+const TRIANGLE = 1;
+const CIRCLE = 2;
+
+let g_selectedColor=[1.0,1.0,1.0,1.0];
+let g_selectedSize = 5;
+let g_selectedType = POINT;
+let g_circleSegments = 3;
+
+function addActionsForHtmlUI(){
+  document.getElementById('green').onclick = function() { g_selectedColor = [0.0,1.0,0.0,1.0]; };
+  document.getElementById('red').onclick = function() { g_selectedColor = [1.0,0.0,0.0,1.0]; };
+  document.getElementById('clearButton').onclick = function() { g_shapeList=[]; renderAllShapes(); };
+
+  document.getElementById('pointButton').onclick = function() {g_selectedType=POINT};
+  document.getElementById('triButton').onclick = function() {g_selectedType=TRIANGLE};
+  document.getElementById('circleButton').onclick = function() {g_selectedType=CIRCLE};
+  document.getElementById('drawButton').onclick = function() {draw();};
+
+  document.getElementById('redSlide').addEventListener('mouseup',  function() { g_selectedColor[0] = this.value/100; });
+  document.getElementById('greenSlide').addEventListener('mouseup',  function() { g_selectedColor[1] = this.value/100; });
+  document.getElementById('blueSlide').addEventListener('mouseup',  function() { g_selectedColor[2] = this.value/100; });
+
+  document.getElementById('sizeSlide').addEventListener('mouseup',  function() { g_selectedSize = this.value; });
+  document.getElementById('segmentSlide').addEventListener('mouseup', function() { g_circleSegments = this.value; });
+}
+
+function main() {
+  setupWebGL();
+  connectVariablesToGLSL();
+  addActionsForHtmlUI();
+
   // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position, u_FragColor) };
+  canvas.onmousedown = click;
+  canvas.onmousemove = function(ev) { if(ev.buttons == 1) { click(ev)} };
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -56,9 +108,36 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-var g_points = [];  // The array for the position of a mouse press
-var g_colors = [];  // The array to store the color of a point
-function click(ev, gl, canvas, a_Position, u_FragColor) {
+
+
+var g_shapeList = [];
+
+//var g_points = [];  // The array for the position of a mouse press
+//var g_colors = [];  // The array to store the color of a point
+//var g_sizes = [];
+
+function click(ev) {
+  [x,y] = convertCoordinatesEventToGL(ev);
+
+  let point;
+  if(g_selectedType == POINT){
+    point = new Point();
+  } else if (g_selectedType == TRIANGLE){
+    point = new Triangle();
+  } else{
+    point = new Circle();
+    point.segments = g_circleSegments;
+  }
+  point.position = [x,y];
+  point.color = g_selectedColor.slice();
+  point.size = g_selectedSize;
+  g_shapeList.push(point);
+
+  
+  renderAllShapes();
+}
+
+function convertCoordinatesEventToGL(ev){
   var x = ev.clientX; // x coordinate of a mouse pointer
   var y = ev.clientY; // y coordinate of a mouse pointer
   var rect = ev.target.getBoundingClientRect();
@@ -66,30 +145,61 @@ function click(ev, gl, canvas, a_Position, u_FragColor) {
   x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
   y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
 
-  // Store the coordinates to g_points array
-  g_points.push([x, y]);
-  // Store the coordinates to g_points array
-  if (x >= 0.0 && y >= 0.0) {      // First quadrant
-    g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-  } else if (x < 0.0 && y < 0.0) { // Third quadrant
-    g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-  } else {                         // Others
-    g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-  }
+  return([x,y]);
+}
 
+function renderAllShapes(){
+
+  var startTime = performance.now();
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var len = g_points.length;
+  var len = g_shapeList.length;
   for(var i = 0; i < len; i++) {
-    var xy = g_points[i];
-    var rgba = g_colors[i];
-
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
+    g_shapeList[i].render();
   }
+  
+  var duration = performance.now() - startTime;
+  sendTextToHTML("numdot: " + len + " ms: " + Math.floor(duration) + " fps: " + Math.floor(10000/duration)/10, "numdot");
+}
+
+function sendTextToHTML(text, htmlID){
+  var htmlElm = document.getElementById(htmlID);
+  if(!htmlElm){
+    console.log("Failed to get " + htmlID + "from HTML");
+    return;
+  }
+  htmlElm.innerHTML = text;
+}
+
+function draw() {
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+  gl.uniform4f(u_FragColor, 1.0, 1.0, 0.0, 1.0);
+  let cx = 0.7, cy = 0.7, r = 0.15;
+  for (let i = 0; i < 360; i += 30) {
+    let rad1 = i * Math.PI / 180;
+    let rad2 = (i + 30) * Math.PI / 180;
+    let x1 = cx + r * Math.cos(rad1);
+    let y1 = cy + r * Math.sin(rad1);
+    let x2 = cx + r * Math.cos(rad2);
+    let y2 = cy + r * Math.sin(rad2);
+    drawTriangle([cx, cy, x1, y1, x2, y2]);
+  }
+
+  gl.uniform4f(u_FragColor, 0.55, 0.27, 0.07, 1.0); 
+  drawTriangle([-0.1, -0.8, -0.05, -0.4, 0.0, -0.8]);
+  drawTriangle([-0.05, -0.4, 0.05, -0.4, 0.0, -0.8]);
+
+  gl.uniform4f(u_FragColor, 0.0, 0.6, 0.0, 1.0);
+  drawTriangle([-0.15, -0.4, 0.0, -0.2, 0.15, -0.4]);
+  drawTriangle([-0.13, -0.3, 0.0, -0.1, 0.13, -0.3]);
+  drawTriangle([-0.11, -0.2, 0.0, 0.0, 0.11, -0.2]);
+
+  gl.uniform4f(u_FragColor, 0.0, 0.8, 0.0, 1.0); 
+  drawTriangle([-0.8, -0.9, -0.75, -0.75, -0.7, -0.9]);
+  drawTriangle([-0.05, -0.9, 0.0, -0.75, 0.05, -0.9]);
+  drawTriangle([0.6, -0.9, 0.65, -0.75, 0.7, -0.9]);
 }
